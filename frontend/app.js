ea58a018bb30cf8b44f43b1bb00b3f971036bf86
Sofia Sivilotti <68237440+ssivilotti@users.noreply.github.com>
2022-10-14 14:13:15 -0500

    add frontend outline

---
+++ frontend/app.js
@@ -0,0 +1,12 @@
+//Set elements from HTML file
+let read_img_button = document.getElementById("read_image");
+
+let document_contets = document.getElementById("result");
+
+// Add event listener to run naive bayes function
+read_img_button.addEventListener("click", convertToXML);
+let img_upload = document.getElementById("read_image");
+
+// Called to run the backend
+// Loads the image and converts it to an xml file
+function convertToXML() {}
