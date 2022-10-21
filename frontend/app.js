3bf6c406a84a5479132e1150427131c7a1777b2b
Sofia Sivilotti <68237440+ssivilotti@users.noreply.github.com>
2022-10-21 11:18:41 -0500

    add download button to download xml

--- frontend/app.js
+++ frontend/app.js
@@ -9,4 +9,22 @@ let img_upload = document.getElementById("read_image");
 
 // Called to run the backend
 // Loads the image and converts it to an xml file
-function convertToXML() {}
+function convertToXML() {
+  //read image
+  let file = document.getElementById("upload_image").files[0];
+  console.log(file);
+  let reader = new FileReader();
+  reader.readAsArrayBuffer(file);
+  //let image = document.createElement("p");
+  console.log(reader.result);
+  //document_contets.append(image);
+
+  //send image to backend (preps, processes, returns xml)
+
+  //adds link to download file, link appears on screen and when clicked downloads an xml to the computer
+  let link = document.createElement("a");
+  link.href = "../AlgorithmicExample.musicxml";
+  link.setAttribute("download", "");
+  link.innerHTML = "download xml file";
+  document_contets.append(link);
+}
