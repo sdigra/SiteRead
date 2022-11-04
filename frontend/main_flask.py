1101786ad9d71985207f594d8839b1c28679a1e0
Sofia Sivilotti <68237440+ssivilotti@users.noreply.github.com>
2022-11-04 13:53:03 -0500

    add Flask html rendering

--- frontend/main_flask.py
+++ frontend/main_flask.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 # main flask file to run python programs
 #might need to move frontend over to react for better integration with flask
@@ -8,10 +8,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def index():
-    return render_template('index.html')
+def hello_world():
+    try:
+        return render_template('index.html')
+    except Exception as e:
+        return str(e)
 
-app.run(host='0.0.0.0', port=5000, threaded=True, use_reloader=False)
+@app.route('/', methods=['POST'])
+def upload_file():
+    uploaded_file = request.files['upload_image']
+    if uploaded_file.filename != '':
+        uploaded_file.save(uploaded_file.filename)
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+   app.run()
+
 
