e042fdfbc0491234dc5da4979a5aaadd81dd4115
Siya Digra <siyadigra@gmail.com>
2022-12-06 22:54:33 -0600

    Updated xml generation to work w/ coordinates

--- export_to_xml.py
+++ export_to_xml.py
@@ -1,6 +1,7 @@
 from pymusicxml import *
 from music21 import *
 from random import choice, choices
+from nb_train import get_key
 
 def generate_example():
     score = Score(title="Algorithmically Generated MusicXML", composer="HTMLvis")
@@ -31,108 +32,85 @@ def generate_example():
     melody = converter.parse("AlgorithmicExample.musicxml")
     melody.show() # YOU NEED MUSESCORE TO RUN THIS
 
-def coordinate_test():
-    score = Score(title="Coordinate Test", composer="HTMLvis")
-    part = Part("Piano")
-    score.append(part)
-
-    #maps y-coords to notes, this would support c4-b5
-    note_dict  = {0 : "c4", 1 : "d4", 2 : "e4", 3 : "f4", 4 : "g4", 5 : "a5", 6 : "b5"}
-
-    test = [1, 6, 5, 4, 2, 0, 3]
-
-    measures = []
-    m = Measure(time_signature=(3, 4))
-
-    for i in range (len(test)):
-        m = Measure(time_signature=(3, 4) if i == 0 else None)
-        for beat_num in range(3):
-            if (i + beat_num) % 3 == 0:
-                # one quarter note triad
-                m.append(Note(note_dict[test[i]], 1.0))
-            elif (i + beat_num) % 3 == 1:
-                # two eighth note dyads
-                m.append(BeamedGroup([Note(note_dict[test[i]], 0.5) for _ in range(2)]))
-            else:
-                # four 16th note notes
-                m.append(BeamedGroup([Note(note_dict[test[i]], 0.25) for _ in range(4)]))
-        measures.append(m)
-
-    part.extend(measures)
-
-    score.export_to_file("CoordinateTest.musicxml")
-
-    melody = converter.parse("CoordinateTest.musicxml")
-    melody.show() # YOU NEED MUSESCORE TO RUN THIS
-
-
 def generate_from_input(quarter_notes, half_notes, eighth_notes, whole_notes, quarter_rests, half_rests, eighth_rests, whole_rests, sixteenth_rests):
     score = Score(title="Algorithmically Generated MusicXML", composer="HTMLvis")
     part = Part("Piano") #need to receive instrument name from data
     score.append(part)
 
-    pitch_bank = ["f#4", "bb4", "d5", "e5", "ab5", "c6", "f6"]
+    # 1 top of staff, 8 bottom of staff
+    pitch_bank = {1 : "f5", 2: "e5", 3: "d5", 4: "c5", 5: "b4", 6: "a4", 7: "g4", 8: "f4"}
 
     # assume treble clef?
 
     measures = []
-    m = Measure(time_signature=(3, 4))
+    m = Measure(time_signature=(4, 4))
 
     for i in range (len(quarter_notes)):
-        m = Measure(time_signature=(3, 4) if i == 0 else None)
-        for beat_num in range(3):
-            if (i + beat_num) % 3 == 0:
-                # one quarter note triad
-                m.append(Chord(choices(pitch_bank, k=3), 1.0))
+        m = Measure(time_signature=(4, 4) if i == 0 else None)
+        for beat_num in range(4):
+            if (i + beat_num) % 4 == 0:
+                m.append(Note(pitch_bank[get_key(i)], 1.0))
         measures.append(m)
 
-    # for (n, s) in quarter_notes:
-    #     m = Measure(time_signature=(3, 4)) #need to receive time signature from data
-    #     m.append(Note(n, 1.0))
-    # measures.append(m)
+    for i in range (len(half_notes)):
+        m = Measure(time_signature=(4, 4) if i == 0 else None)
+        for beat_num in range(4):
+            if (i + beat_num) % 4 == 0:
+                m.append(Note(pitch_bank[get_key(i)], 2.0))
+        measures.append(m)
 
-    # for (n, s) in half_notes:
-    #     m = Measure(time_signature=(3, 4))
-    #     m.append(Note(n, 2.0))
-    # measures.append(m)
+    for i in range (len(eighth_notes)):
+        m = Measure(time_signature=(4, 4) if i == 0 else None)
+        for beat_num in range(4):
+            if (i + beat_num) % 4 == 0:
+                m.append(Note(pitch_bank[get_key(i)], 0.5))
+        measures.append(m)
 
-    # for (n, s, f) in eighth_notes:
-    #     m = Measure(time_signature=(3, 4))
-    #     m.append(Note(n, 0.5))
-    # measures.append(m)
+    for i in range (len(whole_notes)):
+        m = Measure(time_signature=(4, 4) if i == 0 else None)
+        for beat_num in range(4):
+            if (i + beat_num) % 4 == 0:
+                m.append(Note(pitch_bank[get_key(i)], 4.0))
+        measures.append(m)
 
-    # for (n,) in whole_notes:
-    #     m = Measure(time_signature=(3, 4))
-    #     m.append(Note(n, 4.0))
-    # measures.append(m)
+    for i in range (len(quarter_rests)):
+        m = Measure(time_signature=(4, 4) if i == 0 else None)
+        for beat_num in range(4):
+            if (i + beat_num) % 4 == 0:
+                m.append(Rest(1.0))
+        measures.append(m)
 
-    # for (n,) in quarter_rests:
-    #     m = Measure(time_signature=(3, 4))
-    #     m.append(Rest(n, 1.0))
-    # measures.append(m)
+    for i in range (len(half_rests)):
+        m = Measure(time_signature=(4, 4) if i == 0 else None)
+        for beat_num in range(4):
+            if (i + beat_num) % 4 == 0:
+                m.append(Rest(2.0))
+        measures.append(m)
 
-    # for (n,) in half_rests:
-    #     m = Measure(time_signature=(3, 4))
-    #     m.append(Rest(n, 2.0))
-    # measures.append(m)
+    for i in range (len(eighth_rests)):
+        m = Measure(time_signature=(4, 4) if i == 0 else None)
+        for beat_num in range(4):
+            if (i + beat_num) % 4 == 0:
+                m.append(Rest(0.5))
+        measures.append(m)
 
-    # for (n,) in eighth_rests:
-    #     m = Measure(time_signature=(3, 4))
-    #     m.append(Rest(n, 0.5))
-    # measures.append(m)
+    for i in range (len(whole_rests)):
+        m = Measure(time_signature=(4, 4) if i == 0 else None)
+        for beat_num in range(4):
+            if (i + beat_num) % 4 == 0:
+                m.append(Rest(4.0))
+        measures.append(m)
 
-    # for (n,) in whole_rests:
-    #     m = Measure(time_signature=(3, 4))
-    #     m.append(Rest(n, 4.0))
-    # measures.append(m)
-
-    # for (n,) in sixteenth_rests:
-    #     m = Measure(time_signature=(3, 4))
-    #     m.append(Rest(n, 0.25))
-    # measures.append(m)
+    for i in range (len(sixteenth_rests)):
+        m = Measure(time_signature=(4, 4) if i == 0 else None)
+        for beat_num in range(4):
+            if (i + beat_num) % 4 == 0:
+                m.append(Rest(0.25))
+        measures.append(m)
 
     part.extend(measures)
 
     score.export_to_file("InputGenerated.musicxml")
 
-coordinate_test()
+    melody = converter.parse("InputGenerated.musicxml")
+    melody.show() # YOU NEED MUSESCORE TO RUN THIS
